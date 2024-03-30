b42d465929fb1598fa97171810fe64b618a10478
Damir Nurgaliev <nurgaliev450@gmail.com>
2024-03-30 20:40:01 +0500

    Initial Commit

---
+++ News/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  News
+//
+//  Created by Дамир Нургалиев on 30.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
